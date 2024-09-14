b5584057cd210f0f355e631911f1c008b30c0a75
Amir <amirmehboob921@gmail.com>
2024-09-14 03:34:12 -0700

    update javaScript code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,7 @@
-let toggleBtn = document.querySelector(".navbar__teggler"); // Correct class name
-
-toggleBtn.addEventListener("click", function () {
-  document.querySelectorAll(".sidebar-container").forEach(function (element) {
-    if (element.id === "activated") {
-      element.id = "";
-    } else {
-      element.id = "activated";
-    } // Use classList.toggle properly
-  });
-});
 document.querySelector("#toggler").addEventListener("click", function () {
-  const icon = document.querySelector(".navbar__teggler");
-  if (icon.src.includes("image/bars-solid.svg")) {
-    icon.src = "image/xmark-solid.svg"; // New icon
-  } else {
-    icon.src = "image/bars-solid.svg"; // Original icon
-  }
+  document.querySelector(".sidebar-container").classList.toggle("activated");
+  let img = document.getElementById("toggler__img");
+  img.src = img.src.includes("/image/hamburger.png")
+    ? "/image/cross.png"
+    : "/image/hamburger.png";
 });
